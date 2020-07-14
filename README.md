fb24d9e7af665a955d3c9f5b9050bad67fdafef6
lewis789789 <lewis789789@gmail.com>
2020-07-14 16:57:58 +0800

    20200714

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # myips
+網址:
+http://localhost:3000/users/123
+123可修改
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+  var users = [{ //自己隨便寫一些資料
+    id: 1,
+    name: 'peter'
+  }, {
+    id: 2,
+    name: 'nick'
+  }, {
+    id: 3,
+    name: '\(o_o)/'
+  }];
+  res.render('users', { // 輸出
+    users: users
+  });
+});
+
+router.get('/:id', function (req, res) {
+  res.render('user', { //輸出
+    id: req.params.id
+  })
 });
 
 module.exports = router;
